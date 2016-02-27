5e4d5e443a3084acfb05b1536249f20e8021176d
David Grayson <davidegrayson@gmail.com>
2016-02-26 21:45:24 -0800

    Made it connect to the server and read

--- minecraft-bot/src/main.rs
+++ minecraft-bot/src/main.rs
@@ -3,25 +3,25 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs::File;
 use yaml_rust::YamlLoader;
-use std::net;
 
-//fn read_settings() : yaml_rust::Yaml {
-//}
-
-fn main() {
+fn read_settings() -> yaml_rust::Yaml {
     let mut s = String::new();
-    {
-        let mut f = File::open("config.yml").expect("open config file");
-        f.read_to_string(&mut s).expect("read config file");
-    }
+    let mut f = File::open("config.yml").expect("open config file");
+    f.read_to_string(&mut s).expect("read config file");
     let settings_docs = YamlLoader::load_from_str(&s).unwrap();
     if settings_docs.len() != 1
     {
         panic!("Settings file has wrong number of YAML documents, expected 1.");
     }
     assert_eq!(settings_docs.len(), 1);
-    let ref settings = settings_docs[0];
+    settings_docs[0].to_owned()
+}
+
+fn main() {
+    let settings = read_settings();
     let name : &str = settings["server"].as_str().unwrap();
     println!("Connecting to {}...", name);
     let mut stream = TcpStream::connect(name).unwrap();
+    let mut buffer : [u8; 10] = [0; 10];
+    stream.read(&mut buffer).unwrap();
 }
