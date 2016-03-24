40776e8c9e0af8d8df2e2177830cb07aa2375c43
David Grayson <davidegrayson@gmail.com>
2016-03-23 21:14:33 -0700

    Progress on making a handshake and encoding it.

--- minecraft-bot/src/main.rs
+++ minecraft-bot/src/main.rs
@@ -4,6 +4,23 @@ use std::io::prelude::*;
 use std::fs::File;
 use yaml_rust::YamlLoader;
 
+trait EncodablePacket {
+    fn encode(&self) -> Vec<u8>;
+}
+
+struct Handshake {
+    protocol_version : u32,
+    server_address : String,
+    server_port: u16,
+    next_state : u32,  // 1 for status, 2 for login
+}
+
+impl EncodablePacket for Handshake {
+    fn encode(&self) -> Vec<u8> {
+        vec![1, 2, 3]  // TODO: something real here
+    }
+}
+
 fn read_settings() -> yaml_rust::Yaml {
     let mut s = String::new();
     let mut f = File::open("config.yml").expect("open config file");
@@ -17,16 +34,28 @@ fn read_settings() -> yaml_rust::Yaml {
     settings_docs[0].to_owned()
 }
 
+fn print_bytes(bytes : &[u8]) {
+    for byte in bytes {
+        println!("byte = {}", byte);
+    }
+}
+
 fn main() {
     let settings = read_settings();
-    let name : &str = settings["server"].as_str().unwrap();
+    let server_address : &str = settings["server"].as_str().unwrap();
+    let server_port : u16 = settings["server_port"].as_i64().unwrap() as u16;
+    let name = server_address.to_string() + ":" + &server_port.to_string();
     println!("Connecting to {}...", name);
-    let mut stream = TcpStream::connect(name).unwrap();
+    let mut stream = TcpStream::connect(&*name).unwrap();
+    println!("Connected.");
     let mut buffer : [u8; 10] = [0; 10];
-    let mut handshake = [
-        0,  // packet ID 0: handshake
-        47, // protocol version
-        // TODO: finish this
-        ];
-    stream.read(&mut buffer).unwrap();
+    let mut handshake = Handshake {
+        protocol_version: 47,  // current protocol version
+        server_address: server_address.to_owned(),
+        server_port: server_port,
+        next_state: 2, // login
+    };
+    stream.write(&handshake.encode()).unwrap();
+    let byte_count = stream.read(&mut buffer).unwrap();
+    print_bytes(&buffer[0..byte_count]);
 }
